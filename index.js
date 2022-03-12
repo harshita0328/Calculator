8e1127c5275a9089eff7dc9faf8814f67e461563
harshita0328 <harshitachauhan456@gmail.com>
2022-03-12 08:29:33 +0530

    edit copy rights

---
+++ index.js
@@ -0,0 +1,26 @@
+// OPERATION PERFORM THROUGH FUNCTIONS
+
+let outputScreen = document.getElementById('output') 
+function button(num){
+ outputScreen.value += num;
+}
+
+function calculate(){
+    try{
+        outputScreen.value= eval(outputScreen.value)
+    }
+    catch(err){
+        alert('Invalid')
+    }
+}
+
+function Clear(){
+    outputScreen.value= '';
+}
+
+function del(){
+    outputScreen.value=outputScreen.value.slice(0,-1)
+}
+
+
+
